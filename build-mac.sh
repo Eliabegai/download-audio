a3103ae6288c457b977b527403ad0a7994f4e838
Eliabe Gai <eliabegai@gmail.com>
2024-10-30 11:09:44 -0300

    remove

--- build-mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compila o script com PyInstaller
-pyinstaller --onefile --name=baixar-audio-mac --distpath dist/mac/2_1 ./src/baixar_audio.py
-
-# Aplica a permissão de execução ao arquivo gerado
-chmod +x dist/mac/2_1/baixar-audio-mac
--- build-windows.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Compila o script com PyInstaller
-python -m PyInstaller --onefile --name=baixar-audio-windows --distpath dist/windows/2_1 ./src/baixar_audio.py
